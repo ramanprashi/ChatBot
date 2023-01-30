80d351c33a31605d932f0783f320cf9c9c9e0e35
sagar <5agarm5hetty05@gmail.com>
2023-01-30 21:49:45 +0530

    added a checkbox in side panel

--- client/src/components/ChatMessage.js
+++ client/src/components/ChatMessage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const ChatMessage = ({message}) => {
+const ChatMessage = ({message,textFormat}) => {
 
     return (
         <div className={`chat-msg ${message.user === "gpt" && "bot" }`} >
             <div className={`avatar ${message.user === "gpt" && "bot" }`}>
                 <img src={`../assets/${message.user === "gpt" ? "bot.jpeg" : "user.jpeg"}`} alt='profile-icon' width="40px" height="40px"/>
             </div>
-            <div className="message"><pre>{message.message}</pre></div>
+            <div className="message"><pre className={`${textFormat ? "" : "text-format"}`} >{message.message}</pre></div>
         </div>
     )
 }
